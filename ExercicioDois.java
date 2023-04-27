4d67028ae858ba93ddec0b59d50f284ce505124d
Lucas Ferreira <129918519+LucasAndradeF@users.noreply.github.com>
2023-04-26 21:48:11 -0300

    Add instruções do exercício no código.
    Instruções do exercício e atualização das varíaveis.

--- ExercicioDois.java
+++ ExercicioDois.java
@@ -1,3 +1,7 @@
+//  Escreva um algoritmo que leia o preço de um produto a partir do teclado e apresente na tela o valor do produto acrescido de 10% de seu valor original. 
+// Exemplo:
+// Informe o valor do produto: 50.00
+// Novo valor do produto: 55.00
 import java.util.Scanner;
 
 public class ExercicioDois {
