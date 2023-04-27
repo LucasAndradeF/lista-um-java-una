e32dc8f01b62e212747473eafc97669afe9410c8
Lucas Ferreira <129918519+LucasAndradeF@users.noreply.github.com>
2023-04-26 21:53:35 -0300

    Add instruções do exercício no código.
    Instruções do exercício e atualização das varíaveis.

--- ExercicioTres.java
+++ ExercicioTres.java
@@ -1,20 +1,22 @@
+// Escreva um algoritmo que leia uma temperatura fornecida pelo usuário a partir do teclado em graus Fahrenheit e a converta para o seu equivalente em graus centígrados.
+// OBS.: °C = (°F − 32) / 1,8
 import java.util.Scanner;
 
 public class ExercicioTres {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        double n1, C;
+        double numeroUm, grausCelsius;
 
         String titulo = "Conversor de Temperatura";
         System.out.println(titulo);
 
         System.out.println("Informe a temperatura em Fahrenheit:");
-        n1 = sc.nextInt();
+        numeroUm = sc.nextInt();
         
-        C = (n1 - 32) / 1.8;
+        grausCelsius = (numeroUm - 32) / 1.8;
         
-        System.out.println("A temperatura convertida para graus Celsius é:" + C + " °C");
+        System.out.println("A temperatura convertida para graus Celsius é:" + grausCelsius + " °C");
         sc.close();        
     }
 }
