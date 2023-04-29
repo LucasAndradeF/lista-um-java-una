6c930f6775bc23949aaff2c39a8ae1c95d14f469
Lucas Ferreira <129918519+LucasAndradeF@users.noreply.github.com>
2023-04-29 18:56:12 -0300

    Add orientações do exercício
    Foram adicionadas as orientações para a realização do algoritmo.

--- ExercicioCinco.java
+++ ExercicioCinco.java
@@ -1,3 +1,10 @@
+// Escreva um algoritmo para calcular e imprimir o salário bruto a ser recebido por um funcionário em um mês. 
+// O programa deverá utilizar os seguintes dados:
+// a. Número de horas que o funcionário trabalhou no mês.
+// b. Valor recebido por hora de trabalho.
+// c. Valor do salário família.
+// d. Número de filhos com idade menor que 14 anos, para calcular o salário família.
+
 import java.util.Scanner;
 
 public class ExercicioCinco {
